8a137273543c064e4e4757043c15f641aab9f427
fishdulyapath <fishphatuna@gmail.com>
2023-05-19 10:15:07 +0700

    เพิ่มรายงาน

---
+++ routes/api/purchase/data.js
@@ -0,0 +1,191 @@
+const utils = require("../../../utils");
+
+const printer = require("../../../pdfprinter");
+var nodemailer = require("nodemailer");
+const service = require("./service");
+let moment = require('moment');
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const dataresult = async (token,search) => {
+var resultSet ={success: false, data:null} ;
+  await service.getPurchaseReport(token,search)
+  .then((res) => {
+    console.log(res);
+    if (res.success) {
+     console.log(res.data)
+     resultSet.success = true;
+     resultSet.data = res.data;
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+   const dataset = await resultSet;
+   console.log(dataset);
+   return dataset;
+};
+
+const genPDF = async (body) => {
+  var docDefinition = {
+    content: [
+      {
+        text: "รายงานซื้อสินค้า ",
+        style: "header",
+        alignment: "center",
+      },
+      {
+        style: "tableExample",
+        table: {
+          widths: ["25%", "25%"],
+          body: [
+            [
+              { text: "เอกสารวันที่", alignment: "center" },
+              { text: "เอกสารเลขที่", alignment: "center" },
+            
+            ],
+          ],
+        },
+        layout: "noBorders",
+      },
+      {
+        style: "tableExample",
+        table: {
+          widths: ["25%", "25%"],
+          body: body,
+        },
+        layout: "noBorders",
+      },
+    ],
+    pageOrientation: "landscape",
+    pageMargins: [40, 8, 40, 8],
+    defaultStyle: {
+      font: "Sarabun",
+      fontSize: 12,
+      columnGap: 20,
+      color: "#000",
+    },
+    styles: {
+      header: {
+        bold: true,
+      },
+      textdecoration: {
+        italics: true,
+        alignment: "right",
+        decoration: "underline",
+        decorationStyle: "double",
+      },
+      margindetail: {
+        margin: [20, 0, 0, 0],
+      },
+      margintotal: {
+        margin: [50, 0, 0, 0],
+      },
+    },
+  };
+  return docDefinition;
+};
+
+const genBodyPDF = async (dataset) => {
+  let body = [];
+
+  dataset.forEach((ele) => {
+
+    body.push([
+      { text: utils.formateDate(ele.docdatetime)  },
+      { text: ele.docno },
+    ]);
+  });
+  return body;
+};
+
+const packName = (names)=>{
+  var result = "";
+  for (var i = 0; i < names.length; i++) {
+    if (names[i].name != '') {
+      result += names[i].name;
+      if (i < names.length - 1) {
+        result += ",";
+      }
+    }
+  }
+  return result;
+}
+
+const pdfPreview = async (token,search,res) => {
+  var dataset = await dataresult(token,search);
+  
+  if(dataset.success){
+    var body = await genBodyPDF(dataset.data);
+    var pdfDoc = printer.createPdfKitDocument(await genPDF(body), {});
+    res.setHeader("Content-Type", "application/pdf");
+    pdfDoc.pipe(res);
+    pdfDoc.end();
+  }
+};
+
+const pdfDownload = async (token,search,res) => {
+  var dataset = await dataresult(token,search);
+  var body = await genBodyPDF(dataset.data);
+  var pdfDoc = printer.createPdfKitDocument(await genPDF(body), {});
+  res.setHeader("Content-Type", "application/pdf");
+  res.setHeader("Content-Disposition", 'attachment; filename="balance.pdf"');
+  pdfDoc.pipe(res);
+  pdfDoc.end();
+};
+
+const sendEmail = async (token,emails) => {
+  try {
+    var dataset = await dataresult(token);
+    var body = await genBodyPDF(dataset.data);
+    var pdfDoc = printer.createPdfKitDocument(await genPDF(body), {});
+    pdfDoc.end();
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      secure: false,
+      port: process.env.MAIL_PORT,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+    emails.forEach( (email, index) => {
+      setTimeout(async () => {
+        var name = "fish";
+        console.log("sending email..." + email);
+        let HelperOptions = {
+          from: '"DEDEMerchant Sale Report <admin@smldatacenter.com>',
+          to: email,
+          subject: "Sale Report  from DEDEMerchant",
+          html: "Hello " + name + ",<br><br> Here is your PDF ",
+          attachments: [
+            {
+              filename: "Sale2021.pdf",
+              content: pdfDoc,
+              contentType: "application/pdf",
+            },
+          ],
+        };
+        await transporter.sendMail(HelperOptions, (error, info) => {
+          console.log(info);
+          if (error) {
+            return console.log("error " + error);
+          }
+
+          console.log("The message was sent!");
+       
+        });
+
+        console.log("sending email done");
+      }, index * 1000);
+
+     
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+module.exports = { dataresult, genPDF, pdfPreview, pdfDownload, sendEmail };
