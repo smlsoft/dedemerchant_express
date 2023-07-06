668a0397f269170b161c0f80f118a62296718edd
fishdulyapath <fishphatuna@gmail.com>
2023-07-06 10:59:23 +0700

    check pos pin

---
+++ routes/api/posactive/data.js
@@ -0,0 +1,58 @@
+const utils = require("../../../utils");
+const { createClient } = require("@clickhouse/client");
+const printer = require("../../../pdfprinter");
+var nodemailer = require("nodemailer");
+
+const dotenv = require("dotenv");
+dotenv.config();
+const client = new createClient({
+  host: process.env.CH_SERVER_ADDRESS,
+  username: process.env.CH_USERNAME,
+  password: process.env.CH_PASSWORD,
+  database: process.env.CH_DATABASE_NAME,
+});
+
+const dataresult = async (pin) => {
+  const query = `select pinnumber,isactive,shopid from dede001.posterminal  where pinnumber = '${pin}'`;
+
+  const resultSet = await client.query({
+    query: query,
+    format: "JSONEachRow",
+  });
+  const dataset = await resultSet.json();
+  console.log(dataset);
+  return dataset;
+};
+
+const setActivePos = async (pin, shopid) => {
+  const query = `select pinnumber,isactive,shopid from dede001.posterminal  where pinnumber = '${pin}'`;
+  var result = { success: false, msg: "" };
+  try {
+    const resultSet = await client.query({
+      query: query,
+      format: "JSONEachRow",
+    });
+    const dataset = await resultSet.json();
+
+    if (dataset.length > 0) {
+      const activeQuery = `ALTER TABLE dede001.posterminal UPDATE isactive=1,shopid='${shopid}' where pinnumber = '${pin}'`;
+
+      await client.exec({ query: activeQuery});
+
+      console.log("Data update successful");
+      result.msg = "POS active successful"
+      result.success = true;
+    }else{
+      result.msg = "Pin Number not found"
+      result.false = true;
+    }
+  } catch (error) {
+    console.error(error);
+    result.success = false;
+    result.msg = error.message;
+  }
+  await client.close();
+  return result;
+};
+
+module.exports = { dataresult, setActivePos };
---
+++ routes/api/posactive/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const uuid = require("uuid");
+const utils = require("../../../utils");
+const fs = require("fs");
+const data = require("./data");
+const printer = require("../../../pdfprinter");
+var bodyParser = require("body-parser");
+
+router.get("/", async (req, res) => {
+  try {
+    var where = "";
+
+    var dataset = await data.dataresult(req.query.pin);
+    res.status(200).json({ success: true, data: dataset, msg: "" });
+  } catch (err) {
+    res.status(500).json({ success: false, data: [], msg: err.message });
+  }
+});
+
+router.get("/active", async (req, res) => {
+  try {
+    var dataset = await data.setActivePos(req.query.pin, req.query.shopid);
+    if (dataset.success) {
+      res.status(200).json(dataset);
+    } else {
+      res.status(500).json(dataset);
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, data: [], msg: err.message });
+  }
+});
+
+const _whereLike = async (strKeyWord) => {
+  var __where = "";
+  if (strKeyWord.trim().length > 0) {
+    var __fieldList = ["p.code", "itemname", "unitcost", "unitstandard"];
+    var __keyword = strKeyWord.trim().split(" ");
+    __fieldList.forEach((__fieldList1) => {
+      if (__keyword.length > 0) {
+        if (__where.length > 0) {
+          __where += " OR ";
+        } else {
+          __where += " and ";
+        }
+        __where += "(";
+        for (var __loop = 0; __loop < __keyword.length; __loop++) {
+          if (__loop > 0) {
+            __where += " AND ";
+          }
+          __where += `UPPER(${__fieldList1}) LIKE '%${__keyword[__loop].toUpperCase()}%' `;
+        }
+        __where += ")";
+      }
+    });
+  }
+
+  return __where;
+};
+
+module.exports = router;
