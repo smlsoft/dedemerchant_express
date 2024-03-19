7128416f06bab41db51f23aee826ab0693f1e1eb
fishdulyapath <fishphatuna@gmail.com>
2024-03-19 10:18:55 +0700

    add height

--- routes/api/direction_duration/index.js
+++ routes/api/direction_duration/index.js
@@ -8,7 +8,7 @@ const path = require("path");
 router.get(
   "/",
   utils.catchAsync(async (req, res) => {
-    const { origin, destination, key } = req.query;
+    const { origin, destination, key,width,height } = req.query;
 
     const directionsUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&mode=DRIVING&destination=${destination}&key=${key}`;
 
@@ -22,7 +22,7 @@ router.get(
         const startLocation = route.legs[0].start_location;
         const endLocation = route.legs[0].end_location;
         const polyline = route.overview_polyline.points;
-        const staticMapUrl = `https://maps.googleapis.com/maps/api/staticmap?size=600x300&path=enc:${polyline}&markers=color:green|label:S|${startLocation.lat},${startLocation.lng}&markers=color:red|label:E|${endLocation.lat},${endLocation.lng}&key=${key}`;
+        const staticMapUrl = `https://maps.googleapis.com/maps/api/staticmap?size=${width}x${height}&path=enc:${polyline}&markers=color:green|label:S|${startLocation.lat},${startLocation.lng}&markers=color:red|label:E|${endLocation.lat},${endLocation.lng}&key=${key}`;
         const responseImg = await axios.get(staticMapUrl, { responseType: "arraybuffer" });
         const base64Image = Buffer.from(responseImg.data, "binary").toString("base64");
         res.status(200).json({
